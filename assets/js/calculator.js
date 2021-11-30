2cbc5ae6c99f9f9344f1f847a02139221921b7ff
PetarNovitovic <pnovitovic1@gmail.com>
2021-11-30 16:44:21 +0100

    created popup with opening and closing functionalities, need to make it dinamic

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -45,7 +45,7 @@ ispisMainVehicle()
 // mainVehicleWrapper.innerHTML = a + mainVehicleTypes[0].content;
 
 
-// KREIRANJE STRUKTURE KARTICE
+// SREDJIVANJE GLAVNIH KARTICA
 const bookingIconsMain = document.querySelectorAll(".booking-field-icon-main")
 
 function paintTheCards(index) {
@@ -75,18 +75,116 @@ for (let i = 0; i < bookingIconsMain.length; i++) {
     })
 }
 
+
+// SREDJIVANJE SEKUNDARNIH KARTICA ( sa info box-om )
+const cards = [
+    {
+        id: "name-of-the-card",
+        name: "NameOfTheCarddddd",
+        iconClass: "fas fa-car",
+        content: {
+            imgSrc: "https://tubecabolivia.com/wp-content/uploads/2019/12/imggg.png",
+            text: "Ovo je nas najnoviji i najbolji proizvod"
+        }
+    },
+    {
+        id: "name-of-the-card-2",
+        name: "NameOfTheCard-2",
+        iconClass: "fas fa-ship",
+        content: {
+            imgSrc: "https://tubecabolivia.com/wp-content/uploads/2019/12/imggg.png",
+            text: "Ovo je nas najnoviji i najbolji proizvod"
+        }
+    }
+]
+
+function createSingleCard(obj) {
+    let object = obj;
+
+    let ispis = `
+    <div id="${object.id}" class="single-card">
+        <div class="single-card-inner-cont">
+            <i class="${object.iconClass}"></i>
+            <div class="info-icon">i</div>
+        </div>
+        <p>${object.name}</p>
+    </div>
+    `
+    return ispis;
+}
+
+function displayCardsSelectSize() {
+    let selectSizeWrapper = document.querySelector(".select-size-wrapper")
+
+    for (let i = 0; i < cards.length; i++) {
+        let current = selectSizeWrapper.innerHTML
+        selectSizeWrapper.innerHTML = current + createSingleCard(cards[i])
+    }
+}
+displayCardsSelectSize();
+
+
+
 // Calculator - show fields based on vehicle choose
 let bookingFieldIcon = document.querySelectorAll('.booking-field-icon')
 
+function contentToDisplay(classToBlock) {
+    let niz = [".auto-chosen-options", ".rv-chosen-options", ".boat-chosen-options", ".bike-chosen-options"];
+    let currentClass = classToBlock;
+
+    for (let i = 0; i < niz.length; i++) {
+        document.querySelector(niz[i]).style.display = "none";
+    }
+    for (let i = 0; i < niz.length; i++) {
+        if (niz[i] == currentClass)
+            document.querySelector(currentClass).style.display = "block"
+    }
+}
+
 for (let i = 0; i < bookingFieldIcon.length; i++) {
     bookingFieldIcon[i].addEventListener('click', function () {
-        if (bookingFieldIcon[i].id == 'auto') {
-            document.querySelector('.auto-chosen-options').style.display = "block"
-            document.querySelector('.other-chosen-options').style.display = "none"
-        }
-        else {
-            document.querySelector('.auto-chosen-options').style.display = "none"
-            document.querySelector('.other-chosen-options').style.display = "block"
-        }
+        contentToDisplay("." + bookingFieldIcon[i].id + "-chosen-options");
+
     })
 }
+
+
+// Close the popup window
+let popupX = document.querySelector(".exit-popup")
+
+popupX.addEventListener("click", () => {
+    document.querySelector(".popup-container").style.display = "none"
+})
+
+
+// Show popup on info icon click
+function fillContentPopup() {
+
+}
+
+
+const iksevi = [];
+
+function findXs() {
+    let infoIcons = document.querySelectorAll(".single-card")
+    for (let i = 0; i < infoIcons.length; i++) {
+        iksevi.push(infoIcons[i].children[0].children[1])
+    }
+}
+
+findXs()
+
+
+for (let i = 0; i < iksevi.length; i++) {
+    iksevi[i].addEventListener("click", () => {
+        document.querySelector(".popup-container").style.display = "flex"
+    })
+}
+
+
+
+
+
+
+
+// INITIALIZE FUNCTIONS (functions that start when page is opened)
--- calculator.php
+++ calculator.php
@@ -66,7 +66,13 @@ get_header();
         <div class="booking-calculator"></div>
     </div> -->
 
-
+    <div class="popup-container">
+        <div class="popup-content">
+            <div class="exit-popup">X</div>
+            <img src="https://tubecabolivia.com/wp-content/uploads/2019/12/imggg.png" alt="">
+            <h2>Ovo je nas najnoviji i najbolji proizvod</h2>
+        </div>
+    </div>
     <div class="booking-wrapper booking-wrapper-2 container">
         <div class="booking-form">
             <h2>Complete your booking.</h2>
@@ -78,52 +84,63 @@ get_header();
                     <div class="booking-icons-wrapper"></div>
                 </div>
 
+                <div class="calculator-content-wrapper">
+                    <!-- <div class="single-card">
+                        <div class="single-card-inner-cont">
+                            <i class="fas fa-car"></i>
+                            <div class="info-icon">i</div>
+                        </div>
+                        <p>NameOfTheCard</p>
+                    </div> -->
 
-                <div class="single-card">
-                    <div class="single-card-inner-cont">
-                        <i class="fas fa-car"></i>
-                        <div class="info-icon">i</div>
-                    </div>
-                    <p>NameOfTheCard</p>
-                </div>
+                    <div class="auto-chosen-options choosing-option">
+                        <div class="booking-field size-of-vehicle">
+                            <h3>Select size of your vehicle</h3>
+                            <div class="select-size-wrapper"></div>
+                            <!-- <div class="booking-icons-wrapper">
+                                <div id="2-door-coupe" class="booking-field-icon">2 Door Coupe</div>
+                                <div id="4-door-sedan" class="booking-field-icon">4 Door Sedan</div>
+                                <div id="crossover-mini-suv" class="booking-field-icon">Crossover/Mini SUV</div>
+                                <div id="mid-size-suv" class="booking-field-icon">Mid Size SUV</div>
+                                <div id="large-suv-minivan" class="booking-field-icon">Large SUV/Minivan</div>
+                            </div> -->
+                        </div>
 
-                <div class="auto-chosen-options">
-                    <div class="booking-field size-of-vehicle">
-                        <h3>Select size of your vehicle</h3>
-                        <div class="booking-icons-wrapper">
-                            <div id="2-door-coupe" class="booking-field-icon">2 Door Coupe</div>
-                            <div id="4-door-sedan" class="booking-field-icon">4 Door Sedan</div>
-                            <div id="crossover-mini-suv" class="booking-field-icon">Crossover/Mini SUV</div>
-                            <div id="mid-size-suv" class="booking-field-icon">Mid Size SUV</div>
-                            <div id="large-suv-minivan" class="booking-field-icon">Large SUV/Minivan</div>
+                        <div class="booking-field package">
+                            <h3>Select package</h3>
+                            <div class="select-package-wrapper"></div>
+                            <!-- <div class="booking-icons-wrapper">
+                                <div id="mini-detail" class="booking-field-icon">Mini-detail</div>
+                                <div id="exterior-detail" class="booking-field-icon">Exterior Detail</div>
+                                <div id="interior-detail" class="booking-field-icon">Interior Detail</div>
+                                <div id="full-inside-outside-detail" class="booking-field-icon">Full Inside/Outside Detail</div>
+                                <div id="paint-correction" class="booking-field-icon">Paint Correction</div>
+                                <div id="ceramic-coating" class="booking-field-icon">Ceramic Coating</div>
+                            </div> -->
+                        </div>
+
+                        <div class="booking-field condition">
+                            <h3>Select condition</h3>
+                            <div class="select-condition-wrapper"></div>
+                            <!-- <div class="booking-icons-wrapper">
+                                <div id="new" class="booking-field-icon">New</div>
+                                <div id="used" class="booking-field-icon">Used</div>
+                                <div id="abused" class="booking-field-icon">Abused</div>
+                            </div> -->
                         </div>
                     </div>
 
-                    <div class="booking-field package">
-                        <h3>Select package</h3>
-                        <div class="booking-icons-wrapper">
-                            <div id="mini-detail" class="booking-field-icon">Mini-detail</div>
-                            <div id="exterior-detail" class="booking-field-icon">Exterior Detail</div>
-                            <div id="interior-detail" class="booking-field-icon">Interior Detail</div>
-                            <div id="full-inside-outside-detail" class="booking-field-icon">Full Inside/Outside Detail</div>
-                            <div id="paint-correction" class="booking-field-icon">Paint Correction</div>
-                            <div id="ceramic-coating" class="booking-field-icon">Ceramic Coating</div>
-                        </div>
-                    </div>
+                    <div class="rv-chosen-options choosing-option">rv-chosen-options</div> 
 
-                    <div class="booking-field condition">
-                        <h3>Select condition</h3>
-                        <div class="booking-icons-wrapper">
-                            <div id="new" class="booking-field-icon">New</div>
-                            <div id="used" class="booking-field-icon">Used</div>
-                            <div id="abused" class="booking-field-icon">Abused</div>
-                        </div>
+                    <div class="boat-chosen-options choosing-option">boat-chosen-options</div>   
+
+                    <div class="bike-chosen-options choosing-option">bike-chosen-options</div>                
+
+                    <div class="other-chosen-options">
+                        <p>Other vehicles</p>
                     </div>
                 </div>
-
-                <div class="other-chosen-options">
-                    <p>Other vehicles</p>
-                </div>
+                
 
             </div>
         </div>
