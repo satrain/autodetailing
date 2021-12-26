9f65efc8c932eed26333bd79a07bc9fe271f0f44
satrain <mladen.trickovic5@gmail.com>
2021-12-26 20:22:28 +0100

    display inner pages content

--- page.php
+++ page.php
@@ -6,7 +6,11 @@ get_header();
     <div class="page-header">
         <h1><?php $slug = get_post_field( 'post_name', get_post() ); echo $slug; ?></h1>
     </div>
-
+    <div class="page-content">
+        <div class="container">
+            <?php the_content(); ?>
+        </div>
+    </div>
 </div>
 <?php
 get_footer();
